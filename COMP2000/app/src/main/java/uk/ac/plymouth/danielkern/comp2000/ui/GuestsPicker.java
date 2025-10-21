d5f971f6342a2e0cb891e292a436d17692ce4b2e
NullString1 <kernd2005@gmail.com>
2025-10-21 12:27:47 +0100

    Add guests picker

---
+++ COMP2000/app/src/main/java/uk/ac/plymouth/danielkern/comp2000/ui/GuestsPicker.java
@@ -0,0 +1,44 @@
+package uk.ac.plymouth.danielkern.comp2000.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import uk.ac.plymouth.danielkern.comp2000.R;
+
+public class GuestsPicker extends LinearLayout {
+
+    private EditText totalGuestsI;
+    private EditText childrenI;
+    private EditText highChairsI;
+
+    public GuestsPicker(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public GuestsPicker(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        init(context);
+    }
+
+    public GuestsPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.guests_picker, this, true);
+
+        totalGuestsI = findViewById(R.id.totalGuestsI);
+        childrenI = findViewById(R.id.childrenI);
+        highChairsI = findViewById(R.id.highChairsI);
+
+        totalGuestsI.setText("1");
+        childrenI.setText("0");
+        highChairsI.setText("0");
+
+    }
+}
